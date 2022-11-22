ff90537bcbdbeef7396ef746615f0c3831efa964
Hayden <asianhaydenxd@gmail.com>
2022-11-22 09:01:44 -0500

    facilitate testing format

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod lex;
 
 fn run(file_name: String, code: String) {
-    let tokens = crate::lex::lex(file_name, code);
+    let _tokens = crate::lex::lex(file_name, code);
 }
 
 fn main() {
@@ -13,37 +13,36 @@ fn main() {
 mod tests {
     use crate::lex::*;
 
+    fn test_lexer(code: &str, target: Vec<&str>) {
+        let tokens: Vec<Token> = lex("test".to_string(), code.to_string());
+        for (token, target_string) in tokens.iter().zip(target.iter()) {
+            match token {
+                Token::Identifier(name, _) |
+                Token::Number(name, _) => {
+                    assert_eq!(*name, target_string.to_string(),
+                        "token names \"{}\" and \"{}\" do not match; output tokens are {:?}",
+                        *name, target_string.to_string(), tokens);
+                },
+            }
+        }
+        assert_eq!(tokens.len(), target.len(),
+            "token lengths {} and {} do not match; output tokens are {:?}",
+            tokens.len(), target.len(), tokens);
+    }
+
     #[test]
     fn test() {
-        assert_eq!(
-            lex("main".to_string(), "test".to_string()),
-            vec![Token::Identifier(
-                "test".to_string(),
-                LexContext {
-                    index: 0,
-                    line: 0,
-                    col: 0,
-                    file_name: "main".to_string(),
-                    file_text: "test".to_string(),
-                }
-            )]
-        );
+        test_lexer("test", vec!["test"]);
     }
 
     #[test]
     fn number_lex_test() {
-        assert_eq!(
-            lex("main".to_string(), "1 2 3.0".to_string()),
-            vec![]
-        );
+        test_lexer("1 2 3.0", vec!["1", "2", "3.0"])
     }
 
     #[test]
     fn symbol_lex_test() {
-        assert_eq!(
-            lex("main".to_string(), "1 +".to_string()),
-            vec![]
-        );
+        test_lexer("1 +", vec!["1", "+"]);
     }
 
 }
